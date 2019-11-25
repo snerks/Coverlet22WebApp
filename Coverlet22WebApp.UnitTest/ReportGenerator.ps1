3b25ab3edbdc7af06421b1b3beaf664490f5a88f
snerks <martin.github@outlook.com>
2019-11-25 19:12:07 +0000

    Remove ReportGenerator.ps1

--- Coverlet22WebApp.UnitTest/ReportGenerator.ps1
+++
@@ -1,3 +0,0 @@
-dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
-.\tools\reportgenerator.exe -reports:.\TestResults\coverage.info -targetdir:.\TestResults\
-start .\TestResults\index.htm
