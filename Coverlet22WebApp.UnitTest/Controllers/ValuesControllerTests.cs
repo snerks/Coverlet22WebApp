d6c5c89f7d50e210dcb08487a854c02d16548734
snerks <martin.github@outlook.com>
2019-12-03 21:51:31 +0000

    Fix test

--- Coverlet22WebApp.UnitTest/Controllers/ValuesControllerTests.cs
+++ Coverlet22WebApp.UnitTest/Controllers/ValuesControllerTests.cs
@@ -18,7 +18,7 @@ namespace Coverlet22WebApp.UnitTest.Controllers
             var actual = sut.Get();
 
             // Assert
-            Assert.Null(actual);
+            Assert.NotNull(actual);
         }
     }
 }
